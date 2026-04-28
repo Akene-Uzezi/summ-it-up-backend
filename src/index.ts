b157bfbe50864fda442238510d3f7c2814b93635
Akene Uzezi <xtremejoexj@gmail.com>
2026-04-28 12:16:26 +0100

    wrote a basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import express from "express";
+import { connectRedis } from "./config/redis";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  await connectRedis();
+  console.log(`Server is running on port ${port}`);
+});

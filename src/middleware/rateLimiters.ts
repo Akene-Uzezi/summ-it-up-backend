700884adfd24274309e7dfa257ca945735575152
Akene Uzezi <xtremejoexj@gmail.com>
2026-04-28 14:10:33 +0100

    added the rate limits

---
+++ src/middleware/rateLimiters.ts
@@ -0,0 +1,40 @@
+import { rateLimit } from "express-rate-limit";
+import { RedisStore } from "rate-limit-redis";
+import { redisClient } from "../config/redis";
+
+const userMinuteLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 3,
+  keyGenerator: (req) =>
+    req.ip || req.headers["x-forwarded-for"]?.toString() || "unknown",
+  message:
+    "Too many requests. Please wait a minute before trying to summarize another URL",
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+});
+
+const userHourLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 85,
+  keyGenerator: (req) =>
+    req.ip || req.headers["x-forwarded-for"]?.toString() || "unknown",
+  message:
+    "Too many requests. Please wait an hour before trying to summarize another URL",
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+});
+
+const userDayLimiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 1200,
+  keyGenerator: (req) =>
+    req.ip || req.headers["x-forwarded-for"]?.toString() || "unknown",
+  message: "Limit Reached. Please try again tomorrow.",
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+});
+
+export { userMinuteLimiter, userHourLimiter, userDayLimiter };

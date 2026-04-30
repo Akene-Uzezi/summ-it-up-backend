9e3a0018401d3f8f7cb334523b7d932c1bf7b6e0
Akene Uzezi <xtremejoexj@gmail.com>
2026-04-30 13:57:41 +0100

    set up cors for mobile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors, { CorsOptions } from "cors";
 import { connectRedis } from "./config/redis";
 import { createSummaryRoutes } from "./routes/summary.routes";
 import notFound from "./middleware/notFound";
@@ -10,7 +11,20 @@ import { createLimiters } from "./middleware/rateLimiters";
 const app = express();
 const port = process.env.PORT || 3001;
 
+const allowedOrigins = ["http://localhost:3000", process.env.frontendUrl];
+const options: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(express.json());
+app.use(cors(options));
 
 (async () => {
   await connectRedis();
